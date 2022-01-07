a2b874fd427d86e0fa4197656df3aaa551e426be
ethan <preevete@gmail.com>
2022-01-07 16:15:54 -0700

    working on more tests, end for the night

--- tests/github-info-tests.ts
+++ tests/github-info-tests.ts
@@ -9,5 +9,23 @@ describe('github-info', () => {
         let response = await chai.request(server)
                                     .get('/')
                                     .send()
+        chai.expect(response.status).equal(200)
+
+    })
+    it('get /api/', async () => {
+        let response = await chai.request(server)
+            .get('/api/')
+            .send()
+        chai.expect(response.status).equal(404)
+    })
+    it('get /api/:owner/:repo/', async () => {
+        let response = await chai.request(server)
+            .get('/api/')
+            .send()
+
+        let expectedJson =  {
+
+        }
+        chai.expect(JSON.parse(response.body)).deep.equal(expectedJson)
     })
 })
